3079d9084aa5eb0a91472f91aab05e45bbfbe4db
lithishya <lithishya@gmail.com>
2021-05-19 23:01:09 -0800

    reactiveform and validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -8,20 +9,28 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  uname="";
-  acno="";
-  pswd="";
+ 
+  registerForm = this.fb.group({
+    uname:['',[Validators.required,Validators.pattern('[a-zA-Z]*')]],
+    acno:['',[Validators.required,Validators.minLength(4),Validators.pattern('[0-9]*')]],
+    pswd:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]]
+  })
 
-  constructor(private dataService:DataService,private router:Router) { }
+  constructor(private dataService:DataService,private router:Router,private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
  register()
  {
+   console.log(this.registerForm.get('uname')?.errors);
+   if(this.registerForm.get('uname')?.errors){
+      alert("username required");
+   }
    
-   var uname=this.uname;
-   var acno=this.acno;
-   var pswd=this.pswd;
+  if(this.registerForm.valid){ 
+   var uname=this.registerForm.value.uname;
+   var acno=this.registerForm.value.acno;
+   var pswd=this.registerForm.value.pswd;
   const result= this.dataService.register(uname,acno,pswd)
   if(result){
     alert("successfully registerd....");
@@ -31,4 +40,8 @@ export class RegisterComponent implements OnInit {
     alert("user exist...please login");
   }
  }
+ else{
+   alert("Invalid Form");
+ }
+}
 }
