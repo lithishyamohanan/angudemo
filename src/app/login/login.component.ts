daf9185dc55b80b33ebc3a72ba6109150dd3d2c6
lithishya <lithishya@gmail.com>
2021-05-19 21:05:24 -0800

    dashboard register and login completed with template driven forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +11,8 @@ export class LoginComponent implements OnInit {
   aim="your perfect banking partner";
   acno="account number please";
   pswd="";
-  accountDetails:any = {
-    1000: { acno: 1000,  username: "userone", password: "userone", balance: 50000 },
-    1001: { acno: 1001, username: "usertwo", password: "usertwo", balance: 5000 },
-    1002: { acno: 1002, username: "userthree", password: "userthree", balance: 10000 },
-    1003: { acno: 1003,  username: "userfour", password: "userfour", balance: 6000 }
-}
-  constructor(private router:Router) { }
+ 
+  constructor(private router:Router,private dataService:DataService) { }
 
   ngOnInit(): void {
   }
@@ -32,20 +28,11 @@ login(){
  
   var acno=this.acno;
   var pswd=this.pswd;
-  let users=this.accountDetails;
-  if (acno in users) {
-    if (pswd== users[acno]["password"]) {
-       alert("login successful");
-       this.router.navigateByUrl("dashboard");
-    }
-    else {
-        alert("password incorrect");
-    }
-
-}
-else {
-    alert("Invalid account");
-}
+  const result = this.dataService.login(acno,pswd)
+  if(result){
+    alert("login successful");
+     this.router.navigateByUrl("dashboard");
+  }
 }
 register(){
   this.router.navigateByUrl("register");
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +12,23 @@ export class RegisterComponent implements OnInit {
   acno="";
   pswd="";
 
-  constructor() { }
+  constructor(private dataService:DataService,private router:Router) { }
 
   ngOnInit(): void {
   }
  register()
  {
-   alert("register clicked");
+   
+   var uname=this.uname;
+   var acno=this.acno;
+   var pswd=this.pswd;
+  const result= this.dataService.register(uname,acno,pswd)
+  if(result){
+    alert("successfully registerd....");
+   this.router.navigateByUrl("");
+  }
+  else{
+    alert("user exist...please login");
+  }
  }
 }
